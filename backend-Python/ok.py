260e4aacf407c854cdea3f223dbc4057ed7061b5
FALCOPARISIEN <125257920+FALCOPARISIEN@users.noreply.github.com>
2023-02-14 14:50:29 +0100

    Ok

--- backend-Python/ok.py
+++ backend-Python/ok.py
@@ -1,15 +1,26 @@
 from flask import Flask, jsonify
 
-app = Flask(name)
+app = Flask(__name__)
+
+employees = [
+    {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
+    {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
+    {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
+]
 
 @app.route('/api/v1/employees', methods=['GET'])
-def get_employees():
-    employees = [
-        {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
-        {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
-        {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
-    ]
+def get_all_employees():
     return jsonify(employees)
 
-if name == 'main':
-    app.run(debug=True, port=8080)
+@app.route('/api/v1/employees/<int:id>', methods=['GET'])
+def get_employee_by_id(id):
+    employee = next((emp for emp in employees if emp['id'] == id), None)
+    if employee:
+        return jsonify(employee)
+    else:
+        return jsonify({'error': 'Employee not found'}), 404
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
